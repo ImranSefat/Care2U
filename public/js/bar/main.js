6ffe730783ce84de0078c7d4a495df1772b6baf6
Imran Sefat <imransefat@gmail.com>
2020-05-09 12:30:48 +0600

    Revert "bootstrap min js cdn removed and local file added"
    This reverts commit 4f08afd1c1684ea2eb348694392a49c11f67d962.

--- public/js/bar/main.js
+++ public/js/bar/main.js
@@ -43,7 +43,7 @@ function fillData() {
                 data: patientRecord,
                 // data: dataFromAPI,
                 backgroundColor: [
-                    'rgba(255, 99, 0, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
 
                 ],
                 borderColor: [
@@ -80,8 +80,7 @@ function fillData() {
                 data: recoveredData,
                 // data: dataFromAPI,
                 backgroundColor: [
-                    // rgb(52, 169, 63)
-                    'rgba(52, 169, 63, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
 
                 ],
                 borderColor: [
